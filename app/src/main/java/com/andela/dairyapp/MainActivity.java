c7f6a0fc2cc58cc86601837567796aa82e9841b4
Dany Difo <danydio04@gmail.com>
2019-10-22 15:42:47 +0100

    rien

--- app/src/main/java/com/andela/dairyapp/MainActivity.java
+++ app/src/main/java/com/andela/dairyapp/MainActivity.java
@@ -17,72 +17,64 @@ import com.andela.dairyapp.activities.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-  private ImageView mAppLogo;
-  private TextView mWelcomeText;
-  private View mLayout;
-  private ProgressBar loadingProgressAnim;
-  public static final int ANIM_DURATION = 1500;
+    private ImageView mAppLogo;
+    private TextView mWelcomeText;
+    private View mLayout;
+    private ProgressBar loadingProgressAnim;
+    public static final int ANIM_DURATION = 1500;
 
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    mAppLogo = findViewById(R.id.app_logo);
-    mWelcomeText = findViewById(R.id.intro_message);
-    mLayout = findViewById(R.id.base_layout);
-    loadingProgressAnim = findViewById(R.id.progressBar);
-
-
-    mAppLogo.setVisibility(View.GONE);
-    mWelcomeText.setVisibility(View.GONE);
-
-    launchInitialAnimation();
-
-    new Handler().postDelayed(new Runnable() {
-      @Override
-      public void run() {
-        Intent toHomeActivity = new Intent(MainActivity.this, HomeActivity.class);
-        startActivity(toHomeActivity);
-      }
-    }, 3500);
+        mAppLogo = findViewById(R.id.app_logo);
+        mWelcomeText = findViewById(R.id.intro_message);
+        mLayout = findViewById(R.id.base_layout);
+        loadingProgressAnim = findViewById(R.id.progressBar);
 
 
-  }
+        mAppLogo.setVisibility(View.GONE);
+        mWelcomeText.setVisibility(View.GONE);
+
+        launchInitialAnimation();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent toHomeActivity = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(toHomeActivity);
+            }
+        }, 3500);
 
 
-  private void launchInitialAnimation() {
-
-    ObjectAnimator logoFadeInAnimator = ObjectAnimator.ofFloat(mAppLogo, "alpha", 0.0f, 1.0f);
-    logoFadeInAnimator.setDuration(ANIM_DURATION)
-            .setInterpolator(new AccelerateDecelerateInterpolator());
-
-    // This anim is used for moving the logo downwards
-    ObjectAnimator logoDownwardMovementAnimator = ObjectAnimator.ofFloat(mAppLogo, "translationY", 0f, 200f);
-    logoDownwardMovementAnimator.setDuration(ANIM_DURATION)
-            .setInterpolator(new AccelerateDecelerateInterpolator());
+    }
 
 
-    ObjectAnimator textFadeInAnimator = ObjectAnimator.ofFloat(mWelcomeText, "alpha", 0.0f, 1.0f);
-    textFadeInAnimator.setDuration(ANIM_DURATION);
+    private void launchInitialAnimation() {
 
- ObjectAnimator textDownwardMovementAnimator = ObjectAnimator.ofFloat(mWelcomeText, "translationY", 0f, 200f);
-    textDownwardMovementAnimator.setDuration(ANIM_DURATION);
+        ObjectAnimator logoFadeInAnimator = ObjectAnimator.ofFloat(mAppLogo, "alpha", 0.0f, 1.0f);
+        logoFadeInAnimator.setDuration(ANIM_DURATION)
+                .setInterpolator(new AccelerateDecelerateInterpolator());
 
-    AnimatorSet set = new AnimatorSet();
-    set.playTogether(logoFadeInAnimator, logoDownwardMovementAnimator, textFadeInAnimator, textDownwardMovementAnimator);
-  /*  AnimatorSet sett = new AnimatorSet();
-    sett.playTogether(textFadeInAnimator, textDownwardMovementAnimator);*/
-    set.start();
-//    sett.start();
-    //textFadeInAnimator.start();
-
-    //The code below represents an alternative way of animating the logo and welcome message. It should be used in case above fails.
-
-    mAppLogo.setVisibility(View.VISIBLE);
-    mWelcomeText.setVisibility(View.VISIBLE);
+        // This anim is used for moving the logo downwards
+        ObjectAnimator logoDownwardMovementAnimator = ObjectAnimator.ofFloat(mAppLogo, "translationY", 0f, 200f);
+        logoDownwardMovementAnimator.setDuration(ANIM_DURATION)
+                .setInterpolator(new AccelerateDecelerateInterpolator());
 
 
-  }
+        ObjectAnimator textFadeInAnimator = ObjectAnimator.ofFloat(mWelcomeText, "alpha", 0.0f, 1.0f);
+        textFadeInAnimator.setDuration(ANIM_DURATION);
+
+        ObjectAnimator textDownwardMovementAnimator = ObjectAnimator.ofFloat(mWelcomeText, "translationY", 0f, 200f);
+        textDownwardMovementAnimator.setDuration(ANIM_DURATION);
+
+        AnimatorSet set = new AnimatorSet();
+        set.playTogether(logoFadeInAnimator, logoDownwardMovementAnimator, textFadeInAnimator, textDownwardMovementAnimator);
+        set.start();
+
+        mAppLogo.setVisibility(View.VISIBLE);
+        mWelcomeText.setVisibility(View.VISIBLE);
+    }
 }
